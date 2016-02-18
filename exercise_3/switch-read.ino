9e0a844d2efddd407624ba0970c34a77a546460d
dkappo <dkappo@gmail.com>
2016-02-18 14:42:19 -0600

    Create switch-read.ino

---
+++ exercise_3/switch-read.ino
@@ -0,0 +1,32 @@
+/*
+
+  Read the input from a switch, then make the LED blink once.
+
+*/ 
+
+// Instead of hard-coding the pin numbers, we can use variables and assign their values in one place
+// This makes it much easier to update if we change the pins we're using later
+
+int switchPin = 7;
+int LEDPin = 13;
+
+void setup() {
+
+  pinMode(switchPin,INPUT);
+  pinMode(LEDPin,OUTPUT);
+  digitalWrite(LEDPin,LOW); // Initialize the LED to be off when the program starts
+}
+
+void loop() {
+
+  int switch_value = digitalRead(switchPin);
+  if(switch_value==LOW)
+  {
+   digitalWrite(LEDPin,HIGH);
+   delay(1000);
+   digitalWrite(LEDPin,LOW);
+   delay(1000);
+  }
+  delay(100);
+}
+

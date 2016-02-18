ba857c60ff476be0481d25b78813e0db68e2e504
dkappo <dkappo@gmail.com>
2016-02-18 14:24:26 -0600

    Update blinking_alternately.ino

--- exercise_2/blinking_alternately.ino
+++ exercise_2/blinking_alternately.ino
@@ -5,20 +5,26 @@
 */ 
 
 void setup() {                
+  
 // We'll need 2 pins, one for each LED we want to control independently
+// The pin numbers in the code need to match with the pins you've connected to the breadboard
   pinMode(13, OUTPUT);     
   pinMode(11, OUTPUT);
 }
 
 void loop() {
+  
   // Turn one LED on and turn the other off
   digitalWrite(13, HIGH);
   digitalWrite(11, LOW);
+  
   // Wait 1 second
   delay(1000);
+  
   // Turn the first LED off and the other one on
   digitalWrite(13, LOW);
   digitalWrite(11, HIGH);
+  
   // Wait 1 second
   delay(1000);
 }

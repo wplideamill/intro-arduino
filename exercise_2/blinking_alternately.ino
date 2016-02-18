972005ab1097c81ec3d7e49444a9799e9c81a9bb
dkappo <dkappo@gmail.com>
2016-02-18 14:07:55 -0600

    Create blinking_alternately.ino

---
+++ exercise_2/blinking_alternately.ino
@@ -0,0 +1,25 @@
+/*
+
+  Blinking 2 LEDs alternately
+
+*/ 
+
+void setup() {                
+// We'll need 2 pins, one for each LED we want to control independently
+  pinMode(13, OUTPUT);     
+  pinMode(11, OUTPUT);
+}
+
+void loop() {
+  // Turn one LED on and turn the other off
+  digitalWrite(13, HIGH);
+  digitalWrite(11, LOW);
+  // Wait 1 second
+  delay(1000);
+  // Turn the first LED off and the other one on
+  digitalWrite(13, LOW);
+  digitalWrite(11, HIGH);
+  // Wait 1 second
+  delay(1000);
+}
+

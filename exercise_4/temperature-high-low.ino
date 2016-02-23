62761e62356508c40a58f25db25f96c8866f2273
dkappo <dkappo@gmail.com>
2016-02-23 14:24:02 -0600

    Create temperature-high-low.ino

---
+++ exercise_4/temperature-high-low.ino
@@ -0,0 +1,36 @@
+/*
+
+  Turn red LED on if above 27C, turn green LED on if below 27C
+
+*/ 
+
+int ledRed = 13;
+int ledGreen = 11;
+
+void setup() {
+  pinMode(ledRed,OUTPUT);
+  pinMode(ledGreen,OUTPUT);
+}
+
+void loop() {
+
+    int reading = analogRead(A0);
+    float voltage = reading*(5.0/1024.0);
+    float temperature = (voltage - 0.4)/0.0195;
+
+    if (temperature > 27)
+    {
+      digitalWrite(ledRed,HIGH);
+      digitalWrite(ledGreen,LOW);
+    }
+    else
+    {
+      digitalWrite(ledGreen,HIGH);
+      digitalWrite(ledRed,LOW);
+    }  
+  
+    delay(500);
+    
+}
+
+

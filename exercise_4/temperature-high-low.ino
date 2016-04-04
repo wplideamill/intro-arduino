3da601f4a6ce0ae4714da3e321d07692c5bc6288
Dennis Penner <dkappo@gmail.com>
2016-04-04 15:48:19 -0500

    Update temperature-high-low.ino

--- exercise_4/temperature-high-low.ino
+++ exercise_4/temperature-high-low.ino
@@ -15,7 +15,7 @@ void setup() {
 void loop() {
 
     int reading = analogRead(A0);
-    float voltage = reading*(5.0/1024.0);
+    float voltage = reading*(5.0/1023.0);
     float temperature = (voltage - 0.4)/0.0195;
 
     if (temperature > 27.0)
